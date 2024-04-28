1756546c48f5edcede87f02e4d9fa2bb7334fa71
Foox <72369956+TruFoox@users.noreply.github.com>
2024-04-27 20:19:12 -0700

    Added notice of subreddit to loop error

--- instagram.py
+++ instagram.py
@@ -1,5 +1,4 @@
  ######## CODE MADE BY LANDEN LAFLAMME (TRUFOOX) ########
- 
 import requests, datetime, time, json, sys, math, random, os
 from colorama import Fore, Back, Style
 from PIL import Image
@@ -7,12 +6,10 @@ from io import BytesIO
 from datetime import datetime
 from os import system
 
-
-system("title " + "Instagram Meme Bot") # Sets cmd line window name
-cwd = os.getcwd()
-storageSize = os.path.getsize(f'{cwd}\\used_images.json')
-with open("used_images.json", "r") as infile:
+with open("used_images.json", "r") as infile: # Get used images & file size
     images = json.load(infile)
+    cwd = os.getcwd()
+    storageSize = os.path.getsize(f'{cwd}\\used_images.json')
     print(f"{len(images)} used URLs in storage ({math.floor(storageSize/1000)}kb)")
 
 with open("config.json", "r") as infile: # Starts config
@@ -30,7 +27,7 @@ with open("config.json", "r") as infile: # Starts config
     hashtags = config["Hashtags"]
     debug = config["Debug_Mode"]
 
-# Establish variable 
+# Establish variables & Misc
 media_url = ""
 countattempt = 0
 totalcountattempt = 0
@@ -40,8 +37,9 @@ chosenSubreddit = ""
 upload_url = f"https://graph.facebook.com/v19.0/{user_id}/media"
 publish_url = f"https://graph.facebook.com/v19.0/{user_id}/media_publish"
 tempList = []
+usedDefaultCaption = "False"
 i = 0
-
+system("title " + "Instagram Meme Bot") # Sets cmd line window name
 for subreddit in subreddits: # Janky fix to a bug where the subreddit weights arent being utilized
     n = subredditWeights[i]
     i += 1
@@ -64,9 +62,11 @@ def image_shape(url): # Test image aspect ratio (Whether or not it can fit in in
         delete_previous_line()
         print(Fore.RED + f"Failed to fetch the image from {url}. HTTP Status Code: {response.status_code}")
         return False
+
 def delete_previous_line(): # Highly utilized, used to clear previous lines to make the command line less cluttered/more sleek
     sys.stdout.write('\x1b[1A')
     sys.stdout.write('\x1b[2K') 
+
 if debug == "True":
     print("Debug mode ENABLED")
 currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -91,7 +91,7 @@ while True: # Main code
                 if debug == "True":
                     print(f"Response: {response.json()}")
                 if response.status_code == 200: # Test if request sucessful
-                    if countattempt < 50:
+                    if countattempt < 50: # Ensures the code is not stuck in a loop
                         post_data = response.json()
                         nsfw = post_data.get("nsfw", False)
                         image_url = post_data.get("url", api_url)
@@ -123,7 +123,7 @@ while True: # Main code
                             delete_previous_line()
                             print(Fore.RED + f"The image is marked as NSFW. Trying again... x{countattempt}", flush=True)
                     else:
-                        print(Fore.RED + f"Something is causing a loop - Fix may require reopening this program or waiting for more posts... r/{subreddit}", flush=True)
+                        print(Fore.RED + f"Something is causing a loop with {subreddit}- Fix may require reopening this program or waiting for more posts... r/{subreddit}", flush=True)
                         return ""
                 else:
                     delete_previous_line()
@@ -132,20 +132,21 @@ while True: # Main code
         chosenSubreddit = random.choice(subreddits)
         api_url = f"https://meme-api.com/gimme/{chosenSubreddit}"
         media_url = get_random_image_url(api_url) # Get meme
-        if not media_url == "":
+        if not media_url == "": # There was a loop and does not continue if one occurred
             if defaultCaption == "False" or any(item.lower() in postCaption.lower() for item in captionCriteria): # Set caption to either fallback or post caption
                 upload_data = {
                     "image_url": media_url,
                     "caption": caption + '\n\n.' + '\n\n' + hashtags,
                     "access_token": access_token
                 }
-                defaultCaption == "False"
+                usedDefaultCaption == "False"
             else:
                 upload_data = {
                 "image_url": media_url,
                 "caption": postCaption + '\n\n.' + '\n\n' + hashtags,
                 "access_token": access_token
                 }
+                usedDefaultCaption == "True"
             upload_response = requests.post(upload_url, params=upload_data)
             upload_json = upload_response.json()
             print(Fore.WHITE)
@@ -158,13 +159,13 @@ while True: # Main code
                 print(Fore.RED + f"Error URL: {media_url}")
             else:
                 delete_previous_line()
-                if defaultCaption == "True": # Success messages
+                if usedDefaultCaption == "True": # Success messages
                     print(Fore.GREEN + f"{datetime.now().strftime('%H:%M')} - {media_url} from r/{chosenSubreddit} SUCCESSFULLY uploaded - {countattempt} Attempt(s) - {math.ceil((countsuccess/totalcountattempt)*1000)/10}% Success Rate")
                     print("")
                 else:
                     print(Fore.GREEN + f"{datetime.now().strftime('%H:%M')} - {media_url} from r/{chosenSubreddit} uploaded w/ FALLBACK CAPTION - {countattempt} Attempt(s) - {math.ceil((countsuccess/totalcountattempt)*1000)/10}%")
                     print("")
-                if "id" in upload_json: # I dont remember what this does tbh (Im adding this long after making the code)
+                if "id" in upload_json: # Finishes uploading
                     creation_id = upload_json["id"]
 
                     publish_data = {
@@ -178,3 +179,4 @@ while True: # Main code
     except:
         print(Fore.RED + "CRITICAL ERROR - Whoops, something is wrong! - Please check your config (Check AboutTheConfig.txt)")
         input("Press Enter to exit...")
+        exit()
